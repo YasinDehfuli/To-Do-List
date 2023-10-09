e95707ad987e5cc867279e0a6713a2059c4844f7
YasinDehfuli <ydehfuli@gmail.com>
2023-10-09 17:21:23 +0330

    Add App.js, Create Assignments component and use it

---
+++ components/App.js
@@ -0,0 +1,13 @@
+    import Assignments from "./Assignments.js";
+export default {
+    components:{ Assignments },
+    template: `
+        <div class="grid gap-6">
+            <assignments></assignments>
+          
+        </div>
+ 
+    `,
+
+
+}
